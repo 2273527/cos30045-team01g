7b805191952cf21bc419b57683731e11a02b8f84
chris <2273527@student.swin.edu.au>
2025-05-19 13:48:16 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // COS30045 Data Visualisation
 // Group 01G
 
+// Loading psychiatrists data
+d3.csv("psychiatrists_per_1000_clean.csv").then(function(data) {
+  console.log("Psychiatrists data loaded:");
+  console.log(data); 
+});
 
-
-
+// Loading suicide data
+d3.csv("intentional_self_harm_per_100k_clean.csv").then(function(data) {
+  console.log("Suicide data loaded:");
+  console.log(data); 
+});

0393c0ad3f551d58ed92999c0bb84d87b32021fc
chris <2273527@student.swin.edu.au>
2025-05-21 08:08:59 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // COS30045 Data Visualisation
 // Group 01G
 
-// Loading psychiatrists data
-d3.csv("psychiatrists_per_1000_clean.csv").then(function(data) {
-  console.log("Psychiatrists data loaded:");
-  console.log(data); 
-});
+// Set the size of the SVG area
+var width = 800;
+var height = 500;
+var margin = { top: 50, right: 30, bottom: 100, left: 60 };
 
-// Loading suicide data
-d3.csv("intentional_self_harm_per_100k_clean.csv").then(function(data) {
-  console.log("Suicide data loaded:");
-  console.log(data); 
+// Create the SVG element and add it to the page
+var svg = d3.select("#chart")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+var chartArea = svg.append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var innerWidth = width - margin.left - margin.right;
+var innerHeight = height - margin.top - margin.bottom;
+
+// Load psychiatrists data
+d3.csv("psychiatrists_per_1000_clean.csv").then(function(data) {
+  console.log("CSV loaded:");
+  console.log(data);
 });

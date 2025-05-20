b2177eb4930053339f19365982a6bcea6b1ac279
chris <2273527@student.swin.edu.au>
2025-05-21 08:52:45 +1000

    Update script.js
    Limit chart to first 10 countries to debug overlapping bars

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ d.OBS_VALUE = +d.OBS_VALUE;
 
 // Filter for year 2021
 var data2021 = data.filter(d => d.TIME_PERIOD === "2021");
+data2021 = data2021.slice(0, 10); // TEMP: show only 10 countries
 
 // X and Y scales
 var x = d3.scaleBand()
